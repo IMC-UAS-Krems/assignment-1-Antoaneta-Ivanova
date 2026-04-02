01cfcd815bd4abb7a42a3224c3a3c2cb12579fa0
Antoaneta-Ivanova <25IMC10124@fh-krems.ac.at>
2026-04-02 13:37:36 +0200

    Update artists.py

--- src/streaming/artists.py
+++ src/streaming/artists.py
@@ -6,17 +6,18 @@ Implement the Artist class representing musicians and content creators.
 Classes to implement:
   - Artist
 """
-class Artist:
-        
-        def __init__(self,artist_id: str, name: str, genre: str):
-                self.artist_id = artist_id
-                self.name = name
-                self.genre = genre
-                self.tracks = []
-        
 
-        def add_track(self, track) -> None:
-                pass
-                
-        def track_count(self):
-                pass
+
+class Artist:
+
+    def __init__(self, artist_id: str, name: str, genre: str):
+        self.artist_id = artist_id
+        self.name = name
+        self.genre = genre
+        self.tracks = []
+
+    def add_track(self, track) -> None:
+        self.tracks.append(track)
+
+    def track_count(self) -> int:
+        return len(self.tracks)
