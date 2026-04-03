062ee3583ba62e979a82c6372bb9d94bc95ac600
Antoaneta-Ivanova <25IMC10124@fh-krems.ac.at>
2026-04-03 12:32:21 +0200

    Updated platform.py

--- src/streaming/platform.py
+++ src/streaming/platform.py
@@ -20,45 +20,46 @@ class StreamingPlatform:
 
     def __init__(self, name: str):
         self.name = name
-        self._catalogue = {}
-        self._users = {}
-        self._artists = {}
-        self._albums = {}
-        self._playlists = {}
-        self._sessions = []
+        self._catalogue: dict[str, Track] = {}
+        self._users: dict[str, User] = {}
+        self._artists: dict[str, Artist] = {}
+        self._albums: dict[str, Album] = {}
+        self._playlists: dict[str, Playlist] = {}
+        self._sessions: list[ListeningSession] = []
 
-    def add_track(self, track) -> None:
-        pass
+    def add_track(self, track: Track) -> None:
+        self._catalogue[track.track_id] = track
 
-    def add_user(self, user) -> None:
-        pass
+    def add_user(self, user: User) -> None:
+        self._users[user.user_id] = user
 
-    def add_artist(self, artist) -> None:
-        pass
+    def add_artist(self, artist: Artist) -> None:
+        self._artists[artist.artist_id] = artist
 
-    def add_album(self, album) -> None:
-        pass
+    def add_album(self, album: Album) -> None:
+        self._albums[album.album_id] = album
 
-    def add_playlist(self, playlist) -> None:
-        pass
+    def add_playlist(self, playlist: Playlist) -> None:
+        self._playlists[playlist.playlist_id] = playlist
 
-    def record_session(self, session) -> None:
-        pass
+    def record_session(self, session: ListeningSession) -> None:
+        self._sessions.append(session)
+        session.user.add_session(session)
 
-    def get_track(self, track_id=None):
-        pass
+    def get_track(self, track_id: str) -> Track | None:
+        return self._catalogue.get(track_id)
 
-    def get_user(self, user_id=None):
-        pass
+    def get_user(self, user_id: str) -> User | None:
+        return self._users.get(user_id)
 
-    def get_artist(self, artist_id=None):
-        pass
+    def get_artist(self, artist_id: str) -> Artist | None:
+        return self._artists.get(artist_id)
 
-    def get_album(self, album_id=None):
-        pass
+    def get_album(self, album_id: str) -> Album | None:
+        return self._albums.get(album_id)
 
-    def all_users(self, users):
-        pass
+    def all_users(self) -> list[User]:
+        return list(self._users.values())
 
-    def all_tracks(self, tracks):
-        pass
+    def all_tracks(self) -> list[Track]:
+        return list(self._catalogue.values())
